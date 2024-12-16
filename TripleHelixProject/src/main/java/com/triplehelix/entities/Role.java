b4d6daf82cdffa76578d68a2288c19d87e7a8e24
alesup03 <alessio.suppa@edu.itspiemonte.it>
2024-12-16 16:50:48 +0100

    Update Role with role

--- TripleHelixProject/src/main/java/com/triplehelix/entities/Role.java
+++ TripleHelixProject/src/main/java/com/triplehelix/entities/Role.java
@@ -1,5 +1,16 @@
 package com.triplehelix.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+	
+	@Id
+	private String role_id;
+	
+	private String role_name;
 
 }
