6a28b99a6792b68a04a827a9bfb1f5f9ee53d1c4
castits <lorenzo.castiello@edu.itspiemonte.it>
2024-12-16 12:34:02 +0100

    Spring project added

---
+++ TripleHelixProject/src/main/java/com/trilehelix/TripleHelixProjectApplication.java
@@ -0,0 +1,13 @@
+package com.trilehelix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleHelixProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleHelixProjectApplication.class, args);
+	}
+
+}
