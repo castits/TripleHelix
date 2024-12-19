810ba57e2b9cdbc47cf57e8e4849d301d95126fe
castits <lorenzo.castiello@edu.itspiemonte.it>
2024-12-19 13:48:55 +0100

    UserDAO and UserService configurated

--- TripleHelixProject/src/main/java/com/triplehelix/repos/UserDAO.java
+++ TripleHelixProject/src/main/java/com/triplehelix/repos/UserDAO.java
@@ -1,8 +1,15 @@
 package com.triplehelix.repos;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.triplehelix.entities.User;
+
 @Repository
-public interface UserDAO {
+public interface UserDAO extends JpaRepository<User, Integer> {
+	
+	Optional<User> findUserByEmail(String email);
 
 }
