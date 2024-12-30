7d5f1576041691ef2db205cbfed2f96165623eda
SamueleGallo2002 <samuele.gallo@edu.itspiemonte.it>
2024-12-30 22:01:27 +0100

    Added FeedbackController

---
+++ TripleHelixProject/src/main/java/com/triplehelix/controllers/FeedbackController.java
@@ -0,0 +1,57 @@
+package com.triplehelix.controllers;
+
+import com.triplehelix.entities.Feedback;
+import com.triplehelix.services.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/feedbacks") // Base URL for all feedback-related endpoints
+public class FeedbackController {
+
+    @Autowired
+    private FeedbackService feedbackService; // Inject the FeedbackService
+
+    // Get all feedbacks
+    @GetMapping
+    public ResponseEntity<List<Feedback>> getAllFeedbacks() {
+        List<Feedback> feedbacks = feedbackService.getAllFeedbacks();
+        return new ResponseEntity<>(feedbacks, HttpStatus.OK);
+    }
+
+    // Get feedback by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Feedback> getFeedbackById(@PathVariable int id) {
+        Optional<Feedback> feedback = feedbackService.getFeedbackById(id);
+        return feedback.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // Create new feedback
+    @PostMapping
+    public ResponseEntity<Feedback> createFeedback(@RequestBody Feedback feedback) {
+        Feedback savedFeedback = feedbackService.saveFeedback(feedback);
+        return new ResponseEntity<>(savedFeedback, HttpStatus.CREATED);
+    }
+
+    // Update existing feedback
+    @PutMapping("/{id}")
+    public ResponseEntity<Feedback> updateFeedback(@PathVariable int id, @RequestBody Feedback feedbackDetails) {
+        Optional<Feedback> updatedFeedback = feedbackService.updateFeedback(id, feedbackDetails);
+        return updatedFeedback.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // Delete feedback by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFeedback(@PathVariable int id) {
+        boolean isDeleted = feedbackService.deleteFeedback(id);
+        return isDeleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
