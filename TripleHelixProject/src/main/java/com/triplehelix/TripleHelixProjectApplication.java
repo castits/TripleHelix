9a9c317cb00ede31112f408bd457003dfb749649
castits <lorenzo.castiello@edu.itspiemonte.it>
2025-01-07 15:35:37 +0100

    Email sender configurated

--- TripleHelixProject/src/main/java/com/triplehelix/TripleHelixProjectApplication.java
+++ TripleHelixProject/src/main/java/com/triplehelix/TripleHelixProjectApplication.java
@@ -2,13 +2,27 @@ package com.triplehelix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.triplehelix.schedulers.BookingReminderScheduler;
+
 @SpringBootApplication(scanBasePackages = "com.triplehelix")
+@EnableScheduling
 public class TripleHelixProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripleHelixProjectApplication.class, args);
+		/*
+		var context = SpringApplication.run(TripleHelixProjectApplication.class, args);
+		
+		BookingReminderScheduler reminder = context.getBean(BookingReminderScheduler.class);
+		
+		reminder.sendBookingsReminders();
+		
+		context.close();
+		*/
 	}
 
 }
