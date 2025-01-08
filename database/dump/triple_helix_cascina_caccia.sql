0a6692238ea5dd5cae90c1788ef8092f2a957543
castits <lorenzo.castiello@edu.itspiemonte.it>
2025-01-08 20:38:19 +0100

    DB updated

--- database/dump/triple_helix_cascina_caccia.sql
+++ database/dump/triple_helix_cascina_caccia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 19, 2024 alle 14:56
--- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.2.4
+-- Creato il: Gen 08, 2025 alle 20:35
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,13 +31,20 @@ CREATE TABLE `bookings` (
   `booking_id` int(11) NOT NULL,
   `request_id` int(11) NOT NULL,
   `participant_quantity` int(3) NOT NULL,
-  `appointment_date` datetime NOT NULL,
+  `appointment_date` date NOT NULL,
+  `time_slot` enum('MORNING','AFTERNOON','FULL_DAY') NOT NULL,
+  `status` enum('PENDING','CONFIRMED','REFUSED') NOT NULL,
   `booking_info_req` text DEFAULT NULL,
-  `event_confirmation_sent` tinyint(1) NOT NULL,
   `reminder_sent` tinyint(1) NOT NULL,
-  `feedback_sent` tinyint(1) NOT NULL,
-  `sign_up_confirmation_sent` tinyint(1) NOT NULL
+  `feedback_sent` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `bookings`
+--
+
+INSERT INTO `bookings` (`booking_id`, `request_id`, `participant_quantity`, `appointment_date`, `time_slot`, `status`, `booking_info_req`, `reminder_sent`, `feedback_sent`) VALUES
+(5, 5, 40, '2025-01-11', 'FULL_DAY', 'PENDING', NULL, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -78,11 +85,17 @@ CREATE TABLE `information_request` (
 CREATE TABLE `requests` (
   `request_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `status` varchar(50) NOT NULL,
   `institute` varchar(70) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `requests`
+--
+
+INSERT INTO `requests` (`request_id`, `user_id`, `institute`, `created_at`, `updated_at`) VALUES
+(5, 22, 'ITS ICT', '2025-01-08 19:15:14', '2025-01-08 19:15:14');
 
 -- --------------------------------------------------------
 
@@ -92,8 +105,16 @@ CREATE TABLE `requests` (
 
 CREATE TABLE `roles` (
   `role_id` int(11) NOT NULL,
-  `role_name` varchar(40) NOT NULL
+  `role_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `role_name`) VALUES
+(1, 'ADMIN'),
+(2, 'USER');
 
 -- --------------------------------------------------------
 
@@ -103,15 +124,21 @@ CREATE TABLE `roles` (
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
-  `user_name` varchar(100) NOT NULL,
-  `user_surname` varchar(100) NOT NULL,
-  `user_email` varchar(150) NOT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `user_surname` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
   `user_password` varchar(255) NOT NULL,
-  `user_phone` varchar(20) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_surname`, `user_email`, `user_password`, `created_at`, `updated_at`, `role_id`) VALUES
+(22, 'Lorenzo', 'Castiello', 'lorenzo.castiello04@gmail.com', '$2a$10$B1iSpx/REdXN22yHFs6CQuOfQGL7z5qbHB1yrs0zUEGJQaSxuMaI2', '2025-01-08 16:26:12', '2025-01-08 16:26:12', 1);
 
 --
 -- Indici per le tabelle scaricate
@@ -168,7 +195,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT per la tabella `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT per la tabella `feedbacks`
@@ -186,19 +213,19 @@ ALTER TABLE `information_request`
 -- AUTO_INCREMENT per la tabella `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT per la tabella `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Limiti per le tabelle scaricate
