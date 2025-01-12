e5f7610aad7df4d5ec30bdcc993afd2527621218
Stefano Cherio <stefano.cherio@edu.itspiemonte.it>
2025-01-12 16:17:03 +0100

    added login styling

---
+++ TripleHelixProject/bin/src/main/resources/static/js/registrazione.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("registration-form")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const password = document.getElementById("password").value;
+      const repeatPassword = document.getElementById("conferma-password").value;
+      if (password === repeatPassword) {
+        const response = await fetch("/pub/auth/register", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            userName: document.getElementById("nome").value,
+            userSurname: document.getElementById("cognome").value,
+            userEmail: document.getElementById("email").value,
+            userPassword: document.getElementById("password").value,
+            roleId: 2,
+          }),
+        });
+        if (response.status === 201) {
+          window.location.href = "/login.html";
+        }
+      }
+    });
+});
