9eb8262980d279f3d449aba4cb6226d2472dc531
castits <lorenzo.castiello@edu.itspiemonte.it>
2025-01-12 23:05:06 +0100

    Change booking status by booking id

--- TripleHelixProject/src/main/java/com/triplehelix/schedulers/BookingReminderScheduler.java
+++ TripleHelixProject/src/main/java/com/triplehelix/schedulers/BookingReminderScheduler.java
@@ -1,7 +1,6 @@
 package com.triplehelix.schedulers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
