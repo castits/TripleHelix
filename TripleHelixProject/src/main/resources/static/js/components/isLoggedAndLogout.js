2298064851835798966a62858a68854ff926cae6
Stefano Cherio <stefano.cherio@edu.itspiemonte.it>
2025-01-21 15:01:00 +0100

    changed profile id into class

--- TripleHelixProject/src/main/resources/static/js/components/isLoggedAndLogout.js
+++ TripleHelixProject/src/main/resources/static/js/components/isLoggedAndLogout.js
@@ -3,30 +3,34 @@
  * alla pagina appropriata: dashboard se loggato, login altrimenti.
  */
 function isLogged() {
-  const profile = document.getElementById("profile");
+  const profileIcons = document.querySelectorAll(".profile");
   let isUserLogged = false;
 
-  profile.addEventListener("click", async () => {
-    try {
-      const response = await fetch("/pub/auth/is-logged");
+  profileIcons.forEach((profile) => {
+    profile.addEventListener("click", async (event) => {
+      event.preventDefault();
+      try {
+        const response = await fetch("/pub/auth/is-logged");
 
-      if (response.ok) {
-        const text = await response.text();
-        if (text === "true") {
-          isUserLogged = true;
-          console.log(isUserLogged);
+        if (response.ok) {
+          const text = await response.text();
+          if (text === "true") {
+            isUserLogged = true;
+            console.log(isUserLogged);
+          }
+          if (isUserLogged) {
+            location.href = "./usersDashboard.html";
+          } else {
+            location.href = "./login.html";
+          }
         }
-        if (isUserLogged) {
-          location.href = "./dashboardUtente.html";
-        } else {
-          location.href = "./login.html";
-        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
+    });
   });
 }
+
 /**
  * Gestisce il logout dell'utente, inviando una richiesta POST al server per
  * disconnettere l'utente e reindirizzarlo alla homepage.
