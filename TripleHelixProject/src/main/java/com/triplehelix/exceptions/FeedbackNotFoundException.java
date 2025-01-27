4f11653a65365f2206c0a597532e4a49b5ed1fbf
castits <lorenzo.castiello@edu.itspiemonte.it>
2025-01-27 18:15:56 +0100

    Comments added to custom exceptions

--- TripleHelixProject/src/main/java/com/triplehelix/exceptions/FeedbackNotFoundException.java
+++ TripleHelixProject/src/main/java/com/triplehelix/exceptions/FeedbackNotFoundException.java
@@ -1,7 +1,16 @@
 package com.triplehelix.exceptions;
 
+/**
+ * Custom exception for handling cases where a feedback is not found
+ * Extends the RuntimeException to enable unchecked exception handling
+ */
 public class FeedbackNotFoundException extends RuntimeException {
 	
+    /**
+     * Constructor that accepts a custom error message
+     * This message is passed to the parent RuntimeException class
+     * @param message - the error message that describes the exception
+     */
 	public FeedbackNotFoundException(String message) {
         super(message);
     }
