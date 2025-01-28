89fef8f2539351908cd29d1ba93c7719001257a3
Stefano Cherio <stefano.cherio@edu.itspiemonte.it>
2025-01-28 02:23:44 +0100

    user dashboard uploaded

--- TripleHelixProject/src/main/resources/static/js/backoffice/userBookingConfirmed.js
+++ TripleHelixProject/src/main/resources/static/js/backoffice/userBookingConfirmed.js
@@ -25,16 +25,13 @@ window.addEventListener("DOMContentLoaded", () => {
       }
       const endpointPrenotazioni = `/api/bookings/user?email=${encodeURIComponent(
         userData.userEmail
-      )}&status=PENDING`; // Stato "PENDING" per prenotazioni in attesa di conferma
+      )}&status=CONFIRMED`; // Stato "CONFIRMED" per prenotazioni confermate
       const response = await fetch(endpointPrenotazioni);
       if (!response.ok) {
         throw new Error(`Errore HTTP! stato: ${response.status}`);
       }
       const prenotazioni = await response.json();
-      console.log(
-        "Prenotazioni in attesa di conferma dell'utente:",
-        prenotazioni
-      );
+      console.log("Prenotazioni confermate dell'utente:", prenotazioni);
       let nPrenotazioni = prenotazioni.length;
       // Mostra le prenotazioni
       showPrenotazioni(prenotazioni, nPrenotazioni);
@@ -107,9 +104,9 @@ window.addEventListener("DOMContentLoaded", () => {
       console.error("Contenitore prenotazioni non trovato.");
       return;
     }
-    let h2NumPrenotazione = document.getElementById("inAttesa");
+    let h2NumPrenotazione = document.getElementById("confermate");
     let numPrenotazione = document.createElement("span"); // Crea l'elemento <span>
-    numPrenotazione.textContent = ": " + num;
+    numPrenotazione.textContent = " " + num;
 
     h2NumPrenotazione.appendChild(numPrenotazione);
 
@@ -120,8 +117,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     if (prenotazioni.length === 0) {
       const nessunaPrenotazione = document.createElement("p");
-      nessunaPrenotazione.textContent =
-        "Nessuna prenotazione in attesa di conferma.";
+      nessunaPrenotazione.textContent = "Nessuna prenotazione confermata.";
       listaPrenotazione.appendChild(nessunaPrenotazione);
       return;
     }
